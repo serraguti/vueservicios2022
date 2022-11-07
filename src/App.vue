09772a687aedf827aaa83b0929fbde1d1ee35698
serraguti <pacoserranox@gmail.com>
2022-11-07 12:56:24 +0100

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <EmpleadosDetalle/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import CochesComponent from './components/CochesComponent.vue';
+import EmpleadosDetalle from './components/EmpleadosDetalle.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EmpleadosDetalle
   }
 }
 </script>
---
+++ src/Global.js
@@ -0,0 +1,6 @@
+var Global = {
+    urlcoches: "https://apicochespaco.azurewebsites.net/",
+    urlempleados: "https://apiempleadosfullstack.azurewebsites.net/"
+}
+
+export default Global;
